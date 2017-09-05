854fc2a996e013e9760e9e7095567589aff7bc64
Ates Goral <ates.g@myplanet.com>
2017-09-05 13:12:13 -0400

    Add decoder tests

--- test/index.js
+++ test/index.js
@@ -8,8 +8,10 @@ const CLS_CONTEXT_SPECIFIC = 2;
 const FORM_PRIMITIVE = 0;
 const FORM_CONSTRUCTED = 1;
 
+const TAG_INTEGER = 2;
 const TAG_OCTET_STRING = 4;
 const TAG_NULL = 5;
+const TAG_ENUMERATED = 10;
 const TAG_SEQUENCE = 16;
 
 test('fromTree: universal primitive', (t) => {
@@ -41,6 +43,55 @@ test('fromTree: context-specific primitive', (t) => {
   const mapped = Buffer.from([ 1, 2, 3 ]);
 
   t.deepEqual(asn1Mapper.fromTree(tree, definition), mapped);
+});
+
+test('fromFree: primitive decoding: INTEGER', (t) => {
+  const tree = {
+    cls: CLS_UNIVERSAL,
+    form: FORM_PRIMITIVE,
+    tagCode: TAG_INTEGER,
+    value: Buffer.from([ 0x12, 0x34 ])
+  };
+  const definition = {
+    type: 'INTEGER'
+  };
+  const mapped = 0x1234;
+
+  t.is(asn1Mapper.fromTree(tree, definition), mapped);
+});
+
+test('fromFree: primitive decoding: NULL', (t) => {
+  const tree = {
+    cls: CLS_UNIVERSAL,
+    form: FORM_PRIMITIVE,
+    tagCode: TAG_NULL,
+    value: Buffer.from([])
+  };
+  const definition = {
+    type: 'NULL'
+  };
+  const mapped = true;
+
+  t.is(asn1Mapper.fromTree(tree, definition), mapped);
+});
+
+test('fromFree: primitive decoding: ENUMERATED', (t) => {
+  const tree = {
+    cls: CLS_UNIVERSAL,
+    form: FORM_PRIMITIVE,
+    tagCode: TAG_ENUMERATED,
+    value: Buffer.from([ 2 ])
+  };
+  const definition = {
+    type: 'ENUMERATED',
+    values: [
+      { name: 'one', value: 1 },
+      { name: 'two', value: 2 }
+    ]
+  };
+  const mapped = 'two';
+
+  t.is(asn1Mapper.fromTree(tree, definition), mapped);
 });
 
 test('fromTree: universal constructed: single primitive', (t) => {
